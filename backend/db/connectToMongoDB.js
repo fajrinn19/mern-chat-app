59f3bf9469bda53d46a2a2c6b045b25873bd5bc4
fajrinn19 <alfajrininsani@gmail.com>
2024-11-14 01:45:56 +0800

    Deployment

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
-	}
+  console.log("Attempting to connect to MongoDB with URI:", process.env.MONGODB_URI); // Log tambahan
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error.message);
+  }
 };
 
 export default connectToMongoDB;
